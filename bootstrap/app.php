14c916d93181cd047bf8c8eb805cf38177567937
Pascal HAVELANGE <havelangep@hotmail.com>
2025-02-13 23:45:38 +0100

    - Adding trustProxies hardcoded for the time being :-( - Fixing leftover ResourceRoute

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -12,7 +13,17 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->trustProxies(
+            at: [
+                '192.168.255.157',
+                '10.0.0.0/8',
+                '127.0.0.1',
+            ], 
+            headers: Request::HEADER_X_FORWARDED_FOR |
+                Request::HEADER_X_FORWARDED_HOST |
+                Request::HEADER_X_FORWARDED_PORT |
+                Request::HEADER_X_FORWARDED_PROTO
+        );
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
--- routes/api.php
+++ routes/api.php
@@ -3,15 +3,19 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\CountryController;
 use App\Http\Controllers\LanguageController;
 use App\Http\Controllers\ContextController;
 use App\Http\Controllers\PartnerController;
 use App\Http\Controllers\ItemController;
-use App\Http\Controllers\ContextItemController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::resource('country', CountryController::class)->except([
+    'create', 'edit'
+])->middleware('auth:sanctum');
 
 Route::resource('language', LanguageController::class)->except([
     'create', 'edit'
@@ -29,6 +33,3 @@ Route::resource('item', ItemController::class)->except([
     'create', 'edit'
 ])->middleware('auth:sanctum');
 
-Route::resource('context_item', ContextItemController::class)->except([
-    'create', 'edit'
-])->middleware('auth:sanctum');
