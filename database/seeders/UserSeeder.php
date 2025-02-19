08c372d965aefff20b2a510ee0a9cfc7ce68f388
Pascal HAVELANGE <havelangep@hotmail.com>
2025-02-19 14:04:08 +0100

    Adding API Token in userSeeder for testing purposes

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\User;
+use Illuminate\Support\Str;
 
 class UserSeeder extends Seeder
 {
@@ -13,10 +14,24 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        User::create([
+        User::factory()->create([
+            'name' => 'test_user',
+            'email' => 'test@example.com',
+            'password' => bcrypt(Str::random(12)),
+        ]);
+
+        $user = User::create([
             'name' => 'metanull',
             'email' => 'havelangep@hotmail.com',
             'password' => bcrypt('password'),
         ]);
+        $token = $user->createToken('api-token');
+        $user->save();
+
+        $consoleOutput = new \Symfony\Component\Console\Output\ConsoleOutput();
+        $consoleOutput->getFormatter()->setStyle('yellow', new \Symfony\Component\Console\Formatter\OutputFormatterStyle('yellow', null, array('bold')));
+        $consoleOutput->getFormatter()->setStyle('green', new \Symfony\Component\Console\Formatter\OutputFormatterStyle('green', null, array('bold')));
+        $consoleOutput->writeln("\tAPI Token created for user <yellow>{$user->name}</yellow>: <green>{$token->plainTextToken}</green>");
+
     }
 }
