2def91dd39b37b39d46e07539d600709296c6fcb
Pascal HAVELANGE <havelangep@hotmail.com>
2025-04-03 17:57:24 +0200

    Addin Modle, Event, Resource, Controller to support Picture upload and automatic resize

---
+++ app/Http/Controllers/PictureController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Picture;
+use Illuminate\Http\Request;
+use App\Events\PictureUploaded;
+use App\Http\Resources\PictureResource;
+use Illuminate\Support\Facades\Storage;
+
+class PictureController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return Picture::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'file' => 'required|image|mimes:jpeg,png,jpg|max:16384',
+        ]);
+
+        $file = $request->file('file');
+        $path = $request->file('file')->store('Pictures', 'local');
+        /*$filename = (string) Str::uuid() . '.' . $file->getClientOriginalExtension();
+        $path = $file->storeAs('Pictures', $filename, 'local');*/
+
+        $picture = Picture::create([
+            'path' => $path,
+            'name' => $file->getClientOriginalName(),
+            'extension' => $file->getClientOriginalExtension(),
+            'mime_type' => $file->getMimeType(),
+            'size' => $file->getSize(),
+        ]);
+        PictureUploaded::dispatch($picture);
+        return new PictureResource($picture);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Picture $picture)
+    {
+        return new PictureResource($picture);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Picture $picture)
+    {
+        Storage::delete($picture->path);
+        $picture->delete();
+        return response()->json(null, 204);
+    }
+}
---
+++ app/Listeners/ProcessPictureUploaded.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\PictureUploaded;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Support\Facades\Storage;
+use Intervention\Image\ImageManager;
+use Intervention\Image\Drivers\Gd\Driver;
+
+class ProcessPictureUploaded
+{
+    use InteractsWithQueue;
+
+    /**
+     * Create the event listener.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     */
+    public function handle(PictureUploaded $event): void
+    {
+        $file = $event->picture;
+        $path = Storage::path($file->path);
+
+        if (exif_imagetype($path)) {
+
+            $manager = new ImageManager(
+                new Driver()
+            );
+            $picture = $manager->read($path);
+            $picture->resize(1280, 720, function ($constraint) {
+                $constraint->aspectRatio();
+                $constraint->upsize();
+            });
+            $encoded = $picture->toJpg();
+            $encoded->save($path);
+            // Update the file record with the resized path
+            $file->update(['path' => $path]);
+
+            /*$picture = Image::make($path)->fit(1280, 720);
+            // $resizedPath = 'local/Pictures/' . $file->filename;
+            $resizedPath = $path;
+            $picture->save(Storage::path($resizedPath));
+            
+
+            // Update the file record with the resized path
+            $file->update(['path' => $resizedPath]);
+            */
+        }
+    }
+}
