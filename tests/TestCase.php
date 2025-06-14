cc8fea83e3c216fb7e69cbe9b1eae575a442a99b
Pascal HAVELANGE <havelangep@hotmail.com>
2025-06-14 03:19:39 +0200

    - Enable/fix testing setup - Configure env.testing to use a distinct database   - user *must* call  `php artisan migrate --env=testing` before running `php artisan test` - Adding a basic test case fot eh Country resource endpoint

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,29 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Support\Facades\Artisan;
 
 abstract class TestCase extends BaseTestCase
 {
+    use DatabaseMigrations;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        // Run migrations before each test
+        // Artisan::call('migrate', ['--force' => true, '--env' => 'testing']);
+
+        // // Optionally, you can seed the database
+        // // Artisan::call('db:seed', ['--force' => true, '--env' => 'testing']);
+    }
+    protected function tearDown(): void
+    {
+        // Rollback migrations after each test
+        // Artisan::call('migrate:rollback', ['--force' => true, '--env' => 'testing']);
+
+        parent::tearDown();
+    }
     //
 }
