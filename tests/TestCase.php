c74895755d08173687606f968aa327efffb292eb
Pascal HAVELANGE <havelangep@hotmail.com>
2025-06-14 03:21:52 +0200

    Linting

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,8 +2,8 @@
 
 namespace Tests;
 
-use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 use Illuminate\Support\Facades\Artisan;
 
 abstract class TestCase extends BaseTestCase
@@ -20,6 +20,7 @@ abstract class TestCase extends BaseTestCase
         // // Optionally, you can seed the database
         // // Artisan::call('db:seed', ['--force' => true, '--env' => 'testing']);
     }
+
     protected function tearDown(): void
     {
         // Rollback migrations after each test
