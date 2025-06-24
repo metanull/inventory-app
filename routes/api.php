f45f4b3e5020f2e1e47b7bc7367785edf04043ad
Pascal Havelange <havelangep@hotmail.com>
2025-06-24 15:05:40 +0200

    TEMPORARY - Add routes for mobile clients to obtain and revoke JWT tokens

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,11 @@ use App\Http\Controllers\ProjectController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use Illuminate\Validation\ValidationException;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Http\Response;
+use App\Models\User;
+
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
@@ -84,3 +89,46 @@ Route::resource('available-image', AvailableImageController::class)->except([
 Route::resource('detail', DetailController::class)->except([
     'create', 'edit',
 ])->middleware('auth:sanctum');
+
+
+
+
+/**
+ * Route to issue mobile API tokens for users.
+ * This route allows users to authenticate using their email and password,
+ * and receive a token that can be used for mobile API requests.
+ * 
+ * References: https://laravel.com/docs/12.x/sanctum#issuing-mobile-api-tokens
+ */
+Route::post('mobile/token', function (Request $request) {
+    // return response($request->all());
+     $request->validate([
+        'email' => 'required|email',
+        'password' => 'required',
+        'device_name' => 'required',
+    ]);
+    $user = User::where('email', $request->email)->first();
+    if (! $user || ! Hash::check($request->password, $user->password)) {
+        throw ValidationException::withMessages([
+            'email' => ['The provided credentials are incorrect.'],
+        ]);
+    }
+    return response($user->createToken($request->device_name)->plainTextToken, 201);
+})->name('mobile.token');
+
+/**
+ * Route to wipe all mobile API tokens for the authenticated user.
+ * This route allows users to delete all their existing tokens,
+ * effectively logging them out from all devices.
+ */
+Route::get('mobile/wipe', function (Request $request) {
+    // return response($request->all());
+    // return response($request->user());
+    // return response($request->user()->tokens()->get());
+    // return response(User::where('id', $request->user()->id)->first());
+    return response(User::where('email', $request->user()->email)->first());
+
+    $request->user()->tokens()->delete();
+    return response()->noContent();
+})->name('mobile.wipe')
+  ->middleware('auth:sanctum');
