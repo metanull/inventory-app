81289b8d4aadb6e427416105f9ee28ef004d35e1
Pascal HAVELANGE <havelangep@hotmail.com>
2025-06-24 18:32:34 +0200

    Updates to user Model to properly extend and impelment laravel classes and interfaces

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,14 +4,18 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Auth\Authenticatable;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+# use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Fortify\TwoFactorAuthenticatable;
 use Laravel\Jetstream\HasProfilePhoto;
 use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Database\Eloquent\Model;
 
-class User extends Authenticatable
+class User extends Model implements AuthenticatableContract
 {
+    use Authenticatable;
     use HasApiTokens;
 
     /** @use HasFactory<\Database\Factories\UserFactory> */
