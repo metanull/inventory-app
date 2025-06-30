f3d1ff5b6af9cd8986bbf00fca0d9af053ab0d70
Pascal HAVELANGE <havelangep@hotmail.com>
2025-06-30 09:31:15 +0200

    Add TagSeeder and ContextualizationSeeder to `artisan db:seed`

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,6 +19,8 @@ class DatabaseSeeder extends Seeder
             CountrySeeder::class,
             ContextSeeder::class,
             ProjectSeeder::class,
+            TagSeeder::class,
+            ContextualizationSeeder::class,
         ]);
     }
 }
