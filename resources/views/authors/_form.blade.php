8ab9ac963ae86f9ca94fc9c1cc4dcc284eee0760
Pascal HAVELANGE <havelangep@hotmail.com>
2026-04-02 12:21:50 +0200

    fix: add missing author name fields to Blade form, fix Pint import order

--- resources/views/authors/_form.blade.php
+++ resources/views/authors/_form.blade.php
@@ -11,6 +11,38 @@
             />
         </x-form.field>
 
+        <x-form.field label="First Name" name="firstname" variant="gray">
+            <x-form.input 
+                name="firstname" 
+                :value="old('firstname', $author->firstname ?? '')" 
+                placeholder="First name"
+            />
+        </x-form.field>
+
+        <x-form.field label="Last Name" name="lastname" variant="gray">
+            <x-form.input 
+                name="lastname" 
+                :value="old('lastname', $author->lastname ?? '')" 
+                placeholder="Last name"
+            />
+        </x-form.field>
+
+        <x-form.field label="Given Name" name="givenname" variant="gray">
+            <x-form.input 
+                name="givenname" 
+                :value="old('givenname', $author->givenname ?? '')" 
+                placeholder="Given name (if different)"
+            />
+        </x-form.field>
+
+        <x-form.field label="Original Name" name="originalname" variant="gray">
+            <x-form.input 
+                name="originalname" 
+                :value="old('originalname', $author->originalname ?? '')" 
+                placeholder="Name in original script"
+            />
+        </x-form.field>
+
         <x-form.field label="Internal Name" name="internal_name" variant="gray">
             <x-form.input 
                 name="internal_name" 
