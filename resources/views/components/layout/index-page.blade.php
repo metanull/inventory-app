702e1a0d04fa4b91c1c3ceacc8cc2ef0a47ee583
Pascal HAVELANGE <havelangep@hotmail.com>
2026-04-21 13:29:46 +0200

    Add parent-context list Blade primitives
    Fixes #758

--- resources/views/components/layout/index-page.blade.php
+++ resources/views/components/layout/index-page.blade.php
@@ -3,6 +3,7 @@
     'title' => null,
     'createRoute' => null,
     'createButtonText' => null,
+    'parentContext' => null,
 ])
 
 @php
@@ -26,6 +27,19 @@
 
     @if(session('status'))
         <x-ui.alert :message="session('status')" type="success" :entity="$entity" />
+    @endif
+
+    @if(is_array($parentContext) && $parentContext !== [])
+        <x-list.parent-context-header
+            :breadcrumbs="$parentContext['breadcrumbs'] ?? []"
+            :title="$parentContext['title'] ?? null"
+            :description="$parentContext['description'] ?? null"
+            :parent-label="$parentContext['parent_label'] ?? null"
+            :parent-value="$parentContext['parent_value'] ?? null"
+            :parent-url="$parentContext['parent_url'] ?? null"
+            :back-url="$parentContext['back_url'] ?? null"
+            :back-label="$parentContext['back_label'] ?? 'Back'"
+        />
     @endif
 
     {{ $slot }}
